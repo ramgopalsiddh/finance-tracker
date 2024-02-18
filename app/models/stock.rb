41099f5353a1be87ea86ccfdf69e016e349489d5
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-02-19 03:44:08 +0530

    Day 26 Create and Display stock objects in browser

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,12 +1,24 @@
 require 'httparty'
-
+ 
 class Stock < ApplicationRecord
-  def self.new_lookup(ticker_symbol)
-    response = HTTParty.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{ticker_symbol}&apikey=YOUR API KEY")
-      if response.code == 200 && response.parsed_response['Global Quote']
-        response.parsed_response['Global Quote']['05. price']
-      else
-        nil
+ 
+    def self.company_lookup(ticker_symbol)
+        response = HTTParty.get("https://www.alphavantage.co/query?function=OVERVIEW&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alphavantage[:api_key]}")
+        if response.code == 200
+          response.parsed_response['Name']
+        else
+          nil
+        end
+    end
+
+    def self.new_lookup(ticker_symbol)
+        response = HTTParty.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alphavantage[:api_key]}")
+        if response.code == 200 && response.parsed_response['Global Quote']
+          last_price = response.parsed_response['Global Quote']['05. price']
+          name = company_lookup(ticker_symbol)
+          new(ticker: ticker_symbol, name: name, last_price: last_price)
+        else
+          nil
+        end
       end
-  end
 end
