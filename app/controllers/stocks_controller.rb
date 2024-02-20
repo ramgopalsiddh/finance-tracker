94df1beb5455599a326519bea7f808b76ec295e4
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-02-21 02:41:54 +0530

    Day 28 JavaScript responses to invalid search results(show flash alert/message using javascript )

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -8,12 +8,18 @@ class StocksController < ApplicationController
           format.js { render partial: 'users/result_js' }
         end
       else
-        flash[:alert] = "Please enter a valid symbol to search"
-        redirect_to my_portfolio_path
+        
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid symbol to search"
+          format.js { render partial: 'users/result_js' }
+        end
       end
     else
-      flash[:alert] = "Please enter a symbol to search"
-      redirect_to my_portfolio_path
+      
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a symbol to search"
+        format.js { render partial: 'users/result_js' }
+      end
     end
   end
 
