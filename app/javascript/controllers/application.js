767433ccc561967f84ffc4265a850f48bfcac328
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-02-21 02:02:46 +0530

    Day 28 Add Rails UJS (fix for "ActionController::UnknownFormat" error )

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,7 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+import Rails from '@rails/ujs';
+
+Rails.start();
--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 # Add For Bootstrap
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.1/lib/assets/compiled/rails-ujs.js"
