c7cc88d7194f11338212eb2b12a3774c34b9aabb
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-02-21 18:14:37 +0530

    Day 28 Update API request for  get data

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,14 +1,29 @@
 require 'httparty'
  
 class Stock < ApplicationRecord
-  def self.new_lookup(ticker_symbol)
-    response = HTTParty.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alphavantage[:api_key]}")
-    if response.code == 200 && response.parsed_response['Global Quote']
-      last_price = response.parsed_response['Global Quote']['05. price']
-      company_name = response.parsed_response['Global Quote']['01. symbol']
-      new(ticker: ticker_symbol, name: company_name, last_price: last_price)
+
+  def self.company_lookup(ticker_symbol)
+    response = HTTParty.get("https://www.alphavantage.co/query?function=OVERVIEW&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alphavantage[:api_key]}")
+    Rails.logger.info "API Response: #{response.inspect}" # Add this line for debugging
+
+    if response.code == 200
+      response.parsed_response['Name']
     else
       nil
     end
   end
+
+  def self.new_lookup(ticker_symbol)
+    response = HTTParty.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alphavantage[:api_key]}")
+    Rails.logger.info "API Response: #{response.inspect}" # Add this line for debugging
+
+    if response.code == 200 && response.parsed_response['Global Quote']
+      last_price = response.parsed_response['Global Quote']['05. price']
+      name = company_lookup(ticker_symbol)
+      new(ticker: ticker_symbol, name: name, last_price: last_price)
+    else
+      nil
+    end
+  end
+
 end
