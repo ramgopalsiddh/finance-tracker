a4468a34ce4c092ea3988e00174bfca0a32b86a1
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-02-25 12:48:43 +0530

    Day 32 Implement Show password in Login form

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,18 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+
+// function for login form
+document.addEventListener("turbo:load", function() {
+    const password = document.getElementById("password");
+    const showPasswordCheckbox = document.getElementById("show_password_checkbox");
+  
+    if (password && showPasswordCheckbox) {
+        showPasswordCheckbox.addEventListener("change", function() {
+            const showPassword = showPasswordCheckbox.checked;
+  
+            password.type = showPassword ? "text" : "password";
+        });
+    }
+});
+
