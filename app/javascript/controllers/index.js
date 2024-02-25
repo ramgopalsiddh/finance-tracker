119ee3ab1b2ec4730175be847f051710bf681ae2
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-02-25 12:55:12 +0530

    day 32 Implement Show password in Signup & Edit user form

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -24,3 +24,37 @@ document.addEventListener("turbo:load", function() {
     }
 });
 
+
+// function for signup and edit user form
+
+(function() {
+    document.addEventListener("turbo:load", function() {
+      const currentpassword = document.getElementById("current_password");
+      const PasswordCheckbox = document.getElementById("password_checkbox");
+      
+      if (currentpassword && PasswordCheckbox) {
+        PasswordCheckbox.addEventListener("change", function() {
+          const showPassword = PasswordCheckbox.checked;
+          currentpassword.type = showPassword ? "text" : "password";
+        });
+      }
+    });
+  })();
+
+(function() {
+    document.addEventListener("turbo:load", function() {
+      const passwordField = document.getElementById("password_field");
+      const passwordConfirmationField = document.getElementById("password_confirmation_field");
+      const showPasswordCheckbox = document.getElementById("show_password_checkbox");
+
+      if (passwordField && passwordConfirmationField && showPasswordCheckbox) {
+        showPasswordCheckbox.addEventListener("change", function() {
+          const showPassword = showPasswordCheckbox.checked;
+          passwordField.type = showPassword ? "text" : "password";
+          passwordConfirmationField.type = showPassword ? "text" : "password";
+        });
+      } else {
+        console.error("One or more elements not found in the DOM.");
+      }
+    });
+  })();
