b20560de98d5aaa4c8d1de09bb28fb75847690bd
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-03-05 04:33:21 +0530

    Day 41 Update welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
+
+  # show index route without authenticate user
+  skip_before_action :authenticate_user!, only: [:index]
+
   def index
+  # redirect user to my_portfolio_path after login
+  redirect_to my_portfolio_path if user_signed_in?
   end
+
 end
